1f37d2a063d9bc71b536a71b9af9ea690e1e1af2
Ravi-Bukka <ravikumar.bukka@v2solutions.com>
2016-03-10 11:07:26 +0530

    New project for testing

---
+++ LatestSwiftTravis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LatestSwiftTravis
+//
+//  Created by Ravi Bukka on 3/10/16.
+//  Copyright © 2016 v2solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
